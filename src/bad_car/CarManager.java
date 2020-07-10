c546eec3493f0f335756fc0973722d926faa6a9d
beata <beata.choluj@wp.pl>
2020-07-10 16:31:36 +0200

    01ks Add new cars

--- src/bad_car/CarManager.java
+++ src/bad_car/CarManager.java
@@ -14,9 +14,10 @@ public class CarManager {
     List<Car> carsDb = Arrays.asList(
             new Car("1", "Golf III", "Volkswagen"),
             new Car("2", "500", "Fiat"),
-            new Car("3", "Corolla", "Toyota"),
-            new Car("4", "Seicento", "Fiat"),
-            new Car("5", "Rx7", "Mazdaaaaaaaaaa")
+            new Car("3", "Megan", "Renault"),
+            new Car("4", "Corolla", "Toyota"),
+            new Car("5", "Seicento", "Fiat"),
+            new Car("6", "Rx7", "Mazdaaaaaaaaaa")
     );
 
     public Optional<Car> getFromDb(String carId) {
@@ -28,18 +29,18 @@ public class CarManager {
     public String getCarsNames() {
         return carsDb.stream()
                 .map(c -> c.getBrand() + " " + c.getModel())
-                .reduce((prev, next) -> prev + "," + next)
+                .reduce((prev, next) -> prev + ",")
                 .orElse("Sorry. No cars");
     }
 
     public Optional<Car> getBestCar() {
         Map<Car, Integer> carRates = new HashMap<>();
-        Car renault = new Car("3", "Megane", "Renault");
-        Car fiat = new Car("2", "500", "Fiat");
-        Car golf = new Car("1", "Golf III", "Volkswagen");
-        carRates.put(renault, 1);
-        carRates.put(fiat, 2);
-        carRates.put(golf, 3);
+        carRates.put(carsDb.get(0), 1);
+        carRates.put(carsDb.get(1), 2);
+        carRates.put(carsDb.get(2), 3);
+        carRates.put(carsDb.get(3), 4);
+        carRates.put(carsDb.get(4), 5);
+        carRates.put(carsDb.get(5), 6);
         return carsDb.stream()
                 .max((c1, c2) -> Integer.compare(carRates.get(c1), carRates.get(c2)));
     }
